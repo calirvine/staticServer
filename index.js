4d4c81852584fbad8605b3f69f06b1d5381aad32
Cal Irvine <cirvine@Administrators-MacBook-Pro.local>
2019-05-24 11:21:31 -0400

    added some logging

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5050;
